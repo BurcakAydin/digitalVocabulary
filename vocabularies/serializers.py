9e3d61ff098f1a9b2f380fbbd7077869743154e1
Burçak Aydın <burcakaydin@192.168.1.12>
2024-12-12 19:34:39 +0300

    vocabulary app

--- vocabularies/serializers.py
+++ vocabularies/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from vocabularies.models import Vocabulary
+from vocabularies.models import Vocabulary, Word
 
 class VocabularySerializer(serializers.ModelSerializer):
 
     class Meta():
         model = Vocabulary
         fields = ('id', 'name', 'description')
+
+
+class WordSerializer(serializers.ModelSerializer):
+    vocabulary = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Word
+        fields = '__all__'
